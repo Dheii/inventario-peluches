a8d1773ed257b86f924dff344ff4ac2fc47e26aa
Dheii <dhei.longo@gmail.com>
2025-09-03 12:31:24 -0400

    INSTALE LAS TECNOLOGIAS Y LIBRERIAS NECESARIAS POR AHORA, EN MODELOS/modelos,py CREE LOS MODELOS A USAR

---
+++ Modelos/modelos.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, Float
+from models.database import base
+
+class Producto(base):
+
+	_tablename_= "PRODUCTOS"
+
+	id_peluche= Column(Integer, primary_key= True, index= True)
+	nombre= Column(String, nullable= False)
+	categoria= Column(String, nullable=False)
+	tamaño= Column(String, nullable=False)
+	precio_unitario= Column(Float, nullable=False)
+	cantidad_stock= Column(Int, nullable=False)
+	fecha_ingreso= Column(String, nullable= False)
+
+class MOVIMIENTO_INVENTARIO(base):
+	
+	_tablename_= "MOVIMIENTO DE INVENTARIO"
+
+        tipo= Column(String, nullable= False)
+        cantidad= Column(Int, nullable= False)
+        Fecha= Column(Integer, primary_key= index= True)
+        tamaño= Column(String, nullable=False)
