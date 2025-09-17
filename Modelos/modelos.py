58f1d47bc113ff63a1eea5fd5de00e9d9d7727bd
Dheii <dhei.longo@gmail.com>
2025-09-17 10:18:29 -0400

    Actualizar modelos.py

--- Modelos/modelos.py
+++ Modelos/modelos.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
 
-from BdD.basededatos import base# importas la Base que definiste (la nombraste 'base')
+from BdD.basededatos import base
 
 
 class Producto(base):
@@ -9,12 +9,10 @@ class Producto(base):
     id_peluche = Column(Integer, primary_key=True, autoincrement=True, index=True)
     nombre = Column(String(100), nullable=False)
     categoria = Column(String(50), nullable=False)
-    tamano = Column(String(50), nullable=False)          # evité usar 'ñ' en el identificador
+    tamano = Column(String(50), nullable=False)          
     precio_unitario = Column(Float, nullable=False)
     cantidad_stock = Column(Integer, nullable=False, default=0)
     fecha_ingreso = Column(DateTime, nullable=False)
-
-    # Relación opcional con movimientos
     
 
 
@@ -22,6 +20,6 @@ class MovimientoInventario(base):
     __tablename__ = "movimientos_inventario"
 
     id_movimiento = Column(Integer, primary_key=True, autoincrement=True, index=True)
-    tipo = Column(String(20), nullable=False)     # por ejemplo: 'entrada' o 'salida'
+    tipo = Column(String(20), nullable=False)     
     cantidad = Column(Integer, nullable=False)
     fecha = Column(DateTime, nullable=False)
